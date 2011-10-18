ce96f053a4d52bfbe8dbcc046eb317c280edaab7
schneems <richard.schneeman@gmail.com>
2011-10-17 21:05:27 -0500

    ping

--- roman_numeral.rb
+++ roman_numeral.rb
@@ -1,10 +1,28 @@
+
 module RomanNumeral
+  ORDER = { 1000 => "M",
+            500 => "D",
+            100 => "C",
+            50 => "L",
+            10 => "X",
+            5 => "V",
+            1 => "I"}
+
+
   def to_roman
     RomanNumeral.convert_numeric_to_roman self
   end
 
   def self.convert_numeric_to_roman(number)
-    "I"
+    output = ""
+    ORDER.each do |key,value|
+      # puts "key: #{key}: value: #{value}"
+      while number >= key
+        number  = number - key
+        output  = output + value
+      end
+    end
+    output
   end
 end
 
--- spec/roman_numeral_spec.rb
+++ spec/roman_numeral_spec.rb
@@ -4,5 +4,17 @@ describe "RomanNumeral" do
     it "should convert 1 to I" do
       1.to_roman.should == "I"
     end
+    
+    it "should convert 10 to X" do
+      10.to_roman.should == "X"
+    end
+    
+    it "should convert 7 to VII" do
+      7.to_roman.should == "VII"
+    end
+    
+    it "should convert 4 to IV" do
+      4.to_roman.should == "IV"
+    end
   end
 end
